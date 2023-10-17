6329ff201eaff3be6b94eaab6b5ef4fed26d3c8d
Kassia Zheng <nz2280@nyu.edu>
2023-10-17 12:46:59 -0700

    finish

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Week 06
 
-### Classes:
+I found a poem titled 'Love Explained' by Jennifer Michael Hecht on the Poetry Foundation website (https://www.poetryfoundation.org/poems/49250/love-explained). My assignment is divided into three main components:
 
-### Strings:
+Scrolling Verse: The poem itself, which will be presented on the right of the website.
+
+Title Mouse: A feature related to the poem's title.
+
+Transparent Theme Highlight: An element located on the far left side of the webpage.
+
+Additionally, I've incorporated a circular logo to emphasize the theme of love in the background of the love letter.
+
+One of the challenges I encountered during the implementation was ensuring that the text appears both on the left and right sides of the page.
--- classes/sketch.js
+++ classes/sketch.js
@@ -1,34 +1,56 @@
-let circles = [];
-let numCircles = 16;
+let mFont;
+
+let mSize = 30;
+
+let word = "love";
+let newText = "more more more more more more more more more love";
+
+let s0 = "Love Explained";
+
+let phrase =
+  "In idle dreams of lGuy calls the doctor, says the wifes contractions are five minutes apart. Doctor says, Is this her first child? guy says, No, it’s her husband.I promise to try to remember who   I am. Wife gets up on one elbow, says, I wanted to get married. It seemed a fulfillment of some several things, a thing to be done. Even the diamond ring was some thing like a quest, a thing they set you out to get and how insane the quest is; how you have to turn it every way before you can even think to seek it; this metaphysical  refraining is in fact the quest. Who have guessed? She sighs, I like the predictability of two, I like my pleasures fully expected,   when the expectation of them grows patterned in its steady surprise. I’ve got my sweet and tumble pat.t upon a thing like that. Thus explained the woman in contractions to her lover holding on the telephone for the doctor to recover from this strange conversational turn. You say you are whom? It is a pleasure to meet you. She rolls her eyes, but he’d once asked her Am I your first lover? and she would said, Could be. Your face looks familiar. It is the same type of generative error. The grammarof the spoken word will flip, let alone the written, until something new is in us, and in our conversation.";
+  let MARGIN = 40;
+  let currentMaxIndex;
+
+function preload() {
+  mFont = loadFont("./Sniglet Regular.ttf");
+}
 
 function setup() {
-  createCanvas(800, 600);
-  for (let i = 0; i < numCircles; i++) {
-    let myObject = {
-      x: 0,
-      y: random(30, height - 30),
-      v: random(2, 4),
-      r: random(15, 40),
-    };
-    circles.push(myObject);
-  }
+  createCanvas(windowWidth, windowHeight);
+  textFont(mFont);
+  textSize(mSize);
+  textAlign(RIGHT, TOP);
+  fill(250);
+  currentMaxIndex = 1;
 }
 
 function draw() {
-  background("lightblue");
-  fill(220, 10, 100);
+  background(122, 240, 154);
 
-  for (let i = 0; i < circles.length; i += 1) {
-    let myObject = circles[i];
+  let max_xy = frameCount % height;
 
-    // draw circles
-    ellipse(myObject.x, myObject.y, 2 * myObject.r, 2 * myObject.r);
-    myObject.x += myObject.v;
+  for (let xy = 0; xy < max_xy; xy += mSize) {
+    let bbox = mFont.textBounds(word, xy, xy);
+    fill(220, 122, 240);
+    circle(bbox.x+30, bbox.y+15, 30);
+    fill(0);
+    text(word, xy, xy);
 
-    // if reset is needed: pick random diameter and reset x
-    if (myObject.x > width + myObject.r) {
-      myObject.r = random(15, 40);
-      myObject.x = 0 - myObject.r;
-    }
+    let newX = xy + mSize + mSize + mSize;
+    let newY = xy;
+    fill(255,0,0,63);
+    text(newText, newX, newY);
+
+    fill(240, 122, 206);
+    text(s0, mouseX, mouseY);
+
+    fill(74, 54, 68);
+    currentMaxIndex = min(currentMaxIndex + 1, phrase.length);
+
+    let phraseToDraw = phrase.slice(0, currentMaxIndex);
+    text(phraseToDraw, MARGIN, MARGIN, width - 2 * MARGIN, height);
   }
 }
+  
+
